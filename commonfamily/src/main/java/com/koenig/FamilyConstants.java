6f7625f3d94f6a6bc032a3f5a0a61906bee4f9e7
Runje <thomashorn87@gmx.de>
2018-01-12 20:37:51 +0100

    Added ALL_USER Constant

--- commonfamily/src/main/java/com/koenig/FamilyConstants.java
+++ commonfamily/src/main/java/com/koenig/FamilyConstants.java
@@ -1,5 +1,7 @@
 package com.koenig;
 
+import com.koenig.commonModel.User;
+
 import org.joda.time.DateTime;
 
 /**
@@ -10,4 +12,5 @@ public class FamilyConstants {
     public static final long NO_DATE_LONG = 0;
     public static final DateTime NO_DATE = new DateTime(NO_DATE_LONG);
     public static final DateTime UNLIMITED = new DateTime(3000, 12, 31, 23, 59);
+    public static User ALL_USER = new User("ALL", "ALL", "A", new DateTime());
 }
